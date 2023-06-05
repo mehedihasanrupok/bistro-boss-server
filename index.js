92684cf8f544d96f843d3b5c63d7c655ec24560d
mehedihasanrupok <hasanmehedi4114@gmail.com>
2023-06-06 01:11:21 +0600

    Cart DB

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ async function run() {
 
     const menuCollection = client.db("bistroDb").collection("menu");
     const reviewCollection = client.db("bistroDb").collection("reviews");
+    const cartCollection = client.db("bistroDb").collection("carts");
 
     app.get('/menu', async(req, res) =>{
       const result = await menuCollection.find().toArray();
@@ -40,6 +41,15 @@ async function run() {
       const result = await reviewCollection.find().toArray();
       res.send(result);
     })
+
+
+    app.post('/carts', async (req, res) => {
+      const item = req.body;
+      console.log(item);
+      const result = await cartCollection.insertOne(item);
+      res.send(result);
+    })
+
 
 
     // Send a ping to confirm a successful connection
